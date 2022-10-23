b117aec31975a04ed6a469a801d44ff96818e6a8
pravallikadush <pravallikadush@gmail.com>
2022-10-23 17:24:39 +0000

    commiting from remote

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class {
   Public static void main(String [args])
      {
-      System.out.ptintln("Hello!!!! Welcome to devops learning...");
+      System.out.ptintln("Hello!!!! Welcome to devops learning remote...");
      }
 }
 	
